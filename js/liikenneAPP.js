39331894eda617f52e544b2295688c7469f1cbab
Bassenheim <72291136+Bassenheim@users.noreply.github.com>
2023-12-10 10:26:27 +0200

    fixing liikenneAPP123
    asd

--- js/liikenneAPP.js
+++ js/liikenneAPP.js
@@ -1,39 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const liikenneAppContainer = document.getElementById("liikenneAPP");
-  
-    const currentDate = new Date().toISOString().split("T")[0];
+  const liikenneAppContainer = document.getElementById("liikenneAPP");
+  const currentDate = new Date().toISOString().split("T")[0];
 
-    fetch("/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query: `
-          query {
-            getTrainSchedules(
-              departureStation: "HKI",
-              arrivalStation: "DestinationStationCode",
-              departureDate: "${currentDate}",
-              startDate: "${currentDate}T00:00:00Z",
-              endDate: "${currentDate}T23:59:59Z",
-              limit: 10
-            ) {
-                trainNumber
-                departureTime
-                arrivalTime
-            }
+  const asemaValinta = document.getElementById("asemaValinta");
+  const selectedStation = asemaValinta.value;
+
+  fetch("/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          getTrainSchedules(
+            departureStation: "${selectedStation}",
+            arrivalStation: "DestinationStationCode", // Replace with actual destination station code
+            departureDate: "${currentDate}",
+            startDate: "${currentDate}T00:00:00Z",
+            endDate: "${currentDate}T23:59:59Z",
+            limit: 10
+          ) {
+              trainNumber
+              departureTime
+              arrivalTime
           }
-        `,
-      }),
+        }
+      `,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  });
-  
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
